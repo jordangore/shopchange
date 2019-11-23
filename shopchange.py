cd517bf4fef0866ea51db0f6a15b7dab9e62b919
jordangore <57123529+jordangore@users.noreply.github.com>
2019-11-23 14:13:31 -0500

    Create shopchange.py

---
+++ shopchange.py
@@ -0,0 +1,8 @@
+i = 0
+while i<10:
+	price=int(input("Hello! Please enter price:"))
+    tender=int(input("Hello! Please enter amount tendered:"))
+    print(round(tender-(price*1.07,2)))
+    i+=1
+print("Thank you!")
+input()
