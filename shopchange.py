456a066bd350466d4b230936d67b1cca4cfd39f1
jordangore <57123529+jordangore@users.noreply.github.com>
2019-11-23 14:18:06 -0500

    Update shopchange.py

--- shopchange.py
+++ shopchange.py
@@ -1,8 +1,9 @@
 i = 0
 while i<10:
-	price=int(input("Hello! Please enter price:"))
-    tender=int(input("Hello! Please enter amount tendered:"))
+    price=float(input("Hello! Please enter price:"))
+    tender=float(input("Hello! Please enter amount tendered:"))
     print(round(tender-(price*1.07,2)))
     i+=1
 print("Thank you!")
 input()
+
