7f878001b5110b0e598a116d2605e8fe65966f3c
jordangore <57123529+jordangore@users.noreply.github.com>
2019-11-23 21:30:29 -0500

    Update shopchange.py

--- shopchange.py
+++ shopchange.py
@@ -1,7 +1,7 @@
 def shopchange():
     price=float(input("Hello! Please enter price:"))
     tender=float(input("Hello! Please enter amount tendered:"))
-    print(round(tender-(price*1.07,2)))
+    print(round(tender-price*1.07,2))
     end=input('Thank you!\n type "exit" to quit, or press enter to continue')
     if end.lower() == "exit":
         exit()
